020c53c76457aaa28687665e1de29fe903fe04b7
Ahmad-Mohammed <ahmadmo1652@gmail.com>
2024-01-25 11:55:32 +0200

    create orderdetails table

---
+++ database/migrations/2024_01_25_094757_create_orderdetails_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderdetails', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->nullable()->index();
+            $table->string('total-items');
+            $table->string('sub-total');
+            $table->float('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orderdetails');
+    }
+};
