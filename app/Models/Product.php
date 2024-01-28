aac60a7984b1d474797b9453eadffb1e151d6892
unknown <you@example.comgit config --global user.anas Your>
2024-01-28 08:28:12 -0800

    create Model Product

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'slug',
+        'category_id',
+        'price',
+        'organic-certification',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class,'ProductCategory');
+    }
+}
