892bf9c88263304cc6203b8f8b8d16fac4a27da2
MohabsMamdouh <mohabmamdouh22@gmail.com>
2024-01-28 23:21:28 +0200

    update: make OrderItems model

---
+++ app/Models/OrderItems.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItems extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+        'price',
+    ];
+
+    /**
+     * Get the order that owns the OrderItems
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+    /**
+     * Get the product that owns the OrderItems
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+}
