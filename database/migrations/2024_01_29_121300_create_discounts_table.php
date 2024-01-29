9660110b915b2b81c911d4681207635d8897c51c
Ahmad-Mohammed <ahmadmo1652@gmail.com>
2024-01-29 15:00:22 +0200

    create_discounts_ta ble

---
+++ database/migrations/2024_01_29_121300_create_discounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignId('order_id')->nullable()->index();
+            $table->integer('code');
+            $table->string('percentage');
+            $table->date('start_date');
+            $table->date('expiry_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
